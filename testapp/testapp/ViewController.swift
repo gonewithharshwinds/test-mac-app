b82fafb16ba293ebe096584bc9a5a652c835e586
Harsh ADV) <70069185+gonewithharshwinds@users.noreply.github.com>
2021-12-25 22:44:44 +0530

    build 1.0
    test application using swift storyboard - {no further builds}

---
+++ testapp/testapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by Harsh Mehta on 25/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
